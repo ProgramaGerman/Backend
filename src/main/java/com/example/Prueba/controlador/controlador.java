e7b9a4067b126d105c2a1192bdcc8f85c677db9a
German <gownman2004@gmail.com>
2025-07-16 00:58:10 -0400

    compilado

--- src/main/java/com/example/Prueba/controlador/controlador.java
+++ src/main/java/com/example/Prueba/controlador/controlador.java
@@ -1,7 +1,7 @@
+package com.example.Prueba.controlador;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-package com.example.Prueba.controlador;
 
 
 @RestController
