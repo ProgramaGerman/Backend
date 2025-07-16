d6dd4bfb9330de132c3826cb6a2e6e68b244a7e5
German <gownman2004@gmail.com>
2025-07-16 00:49:50 -0400

    comentario

--- src/main/java/com/example/Prueba/controlador/controlador.java
+++ src/main/java/com/example/Prueba/controlador/controlador.java
@@ -9,6 +9,7 @@ public class controlador {
 
     @GetMapping("/saludo")
     public String saludo() {
+        // Este método maneja las solicitudes GET a la ruta /saludo
         return "¡Hola, bienvenido al controlador RESTful!";
     }
 }
